db08f9aa377cdfaeef3700e606fda80e60d85176
Pablo Nieto <pablogabynieto@gmail.com>
2022-01-14 15:47:25 -0300

    desafío complementario y sitio web

---
+++ js/app.js
@@ -0,0 +1,51 @@
+
+
+/*desafío complementario*/
+
+/*Ejemplo 1:*/
+
+let deuda = parseInt(prompt("ingrese el monto de su deuda"));
+for (let acumulación = 1; acumulación <= 12; acumulación++) {
+	let resultado = deuda * acumulación;
+	alert(" Al mes " + acumulación + " acumulara: $" + resultado);
+}
+
+
+/*Ejemplo 2:*/ 
+
+/*let entrada = prompt("MEDIO DE PAGO").toUpperCase();
+
+while(entrada != "ESC" ){
+   switch (entrada) {
+       case "VISA":
+            alert("ACEPTAMOS TARJETA VISA");
+            break;
+        case "MASTERCARD":
+            alert("ACEPTAMOS TARJETA MASTERCARD");
+            break;
+        case "NARANJA":
+            alert("ACEPTAMOS TARJETA NARANJA");
+            break;
+        case "CABAL":
+            alert("ACEPTAMOS TARJETA CABAL");
+            break;
+       default:
+           alert("NO ACEPTAMOS ESE MEDIO DE PAGO");
+           break;
+   }
+   entrada = prompt("INGRESAR UN MEDIO DE PAGO");
+}*/
+
+
+/*Ejemplo 3:*/
+
+/*let num = parseInt(prompt("Ingrese un número"))
+
+while(num <=10) {
+	alert("Hola " + num);
+	num = num + 1
+}
+
+alert("Chau");*/
+
+
