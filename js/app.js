f08bb8413bdc325b184926866ba995bd03f61016
Pablo Nieto <pablogabynieto@gmail.com>
2022-01-26 15:24:29 -0300

    desafío complementario

--- js/app.js
+++ js/app.js
@@ -1,60 +1,71 @@
 
-/*desafío*/
+
+/*desafío complementario*/
+
+/*solicita nombre y apellido al usuario mediante un prompt, con el uso de condicionales para devolver distintos mensajes, dependiendo si llena o no los espacios*/
 
 let nombreIngresado = prompt("Ingresar nombre");
 let apellidoIngresado = prompt("Ingresar apellido");
 
 if ((nombreIngresado !="") && (apellidoIngresado !="")) {
-	alert("Bienvenido/a: "+ nombreIngresado + apellidoIngresado);
+    alert("Bienvenido/a: "+ nombreIngresado + apellidoIngresado);
 }
 
 else {
-	alert("No ingresó ningún dato");
+    alert("No ingresó ningún dato");
 }
 
-let entrada = parseInt(prompt("elegir línea de productos: 1-Yerbas agroecológicas 2-Yerbas blend 3-té en hebras (escriba el número correspondiente a la línea)"));
+/*uso de while para preguntar al usuario si desea comprar y el uso de un prompt para que finalice el ciclo*/
+
+let entrada =prompt("¿Le gustaría comprar?").toLowerCase();
 
 while(entrada != "aceptar"){
    switch (entrada) {
-       case 1:
-            alert("eligió Yerbas agroecológicas");
-            break;
-        case 2:
-            alert("sin stock");
-            break;
-        case 3:
-            alert("sin stock");
+       case "si":
+            alert("Desea comprar");
             break;
        default:
-           alert("No seleccionó ninguna línea");
+           alert("No desea comprar");
            break;
    }
    entrada = prompt("Para continuar escriba 'aceptar'.");
 }
 
-let entrada2 = parseInt(prompt("Si eligió la línea de yerbas agroecológicas seleccione: 1-Yerba Roapipo 2-Yerba Kalena (escriba el número correspondiente al Producto)"));
+/*uso de un array para contener dentro la lista de objetos (mis productos)*/
 
-while(entrada2 != "aceptar"){
-	switch (entrada2) {
-		case 1:
-			alert("Eligió Yerba Roapipo");
-			break;
-		case 2:
-			alert("Eligió Yerba Kalena");
-			break;
-		default:
-			alert("No seleccionó ningún producto");
-			break;
-	}
-	entrada2 = prompt("Para continuar escriba 'aceptar'.");
+
+let stockProductos = [
+    {id: 1, nombre: "Yerba Roapipo", tipo: "yerba agroecológica", precio: 388.43, descuento: 70},
+    {id: 2, nombre: "Yerba Kalena", tipo: "yerba agroecológica", precio: 388.43, descuento: 70},
+    {id: 3, nombre: "Yerba Coffee", tipo: "yerba blend", precio: 669.42, descuento: 100},
+    {id: 4, nombre: "Yerba Flower Power", tipo: "yerba blend", precio: 669.42, descuento: 100},
+    {id: 5, nombre: "Yerba Ginger Mate", tipo: "yerba blend", precio: 669.42, descuento: 100},
+    {id: 6, nombre: "Yerba Tres Mentas", tipo: "yerba blend", precio: 669.42, descuento: 100},
+    {id: 7, nombre: "Té Frutos Rojos", tipo: "té en hebras", precio: 454.545, descuento:50},
+    {id: 8, nombre: "Té Mango", tipo: "té en hebras", precio: 454.545, descuento: 50},
+    {id: 9, nombre: "Té Maracuyá", tipo: "té en hebras", precio: 454.545, descuento: 50},
+    {id: 10, nombre: "Té Chocolate y Menta", tipo: "té en hebras", precio: 454.545, descuento: 50},
+]
+
+let carritoDeCompras = []
+
+agregarAlCarrito()
+
+function agregarAlCarrito() {//esta función permite agregar al carrito mi producto, eligiendo por prompt el "id" correspondiente
+
+    let elijoProducto = parseInt(prompt('ingrese el ID de su producto en caso de querer comprar')) 
+
+    let productoAgregar = stockProductos.find((el) => el.id == elijoProducto)
+    carritoDeCompras.push(productoAgregar)
+     console.log(carritoDeCompras)
+    actualizarCarrito()
+    
 }
 
-
-const suma  = (a,b) => a + b
-const resta = (a,b) => a - b
-const iva = x => x * 0.21
-let valorUnitario  = 300
-let descuento = 70 
-
-let valorFinal = resta(suma(valorUnitario, iva(valorUnitario)), descuento) 
-alert ("Precio total: $" + valorFinal);
+function actualizarCarrito() {//mediante esta función se calcula el valor final del producto con el iva y el descuento incluidos
+    console.log('cantidad de productos agregados ' + carritoDeCompras.length)
+    let suma = carritoDeCompras.reduce((acc, el) => acc + el.precio, 0)
+    let descuento = carritoDeCompras.reduce((acc, el) => acc + el.descuento, 0)
+    let iva = suma * 0.21
+    console.log('la suma total de su Carrito es $' + ((suma + iva) - descuento).toFixed(2))
+}
