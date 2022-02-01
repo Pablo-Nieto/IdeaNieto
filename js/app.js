205f1b22bbf01abffd3764b53c336ef217853a52
Pablo Nieto <pablogabynieto@gmail.com>
2022-02-01 14:32:40 -0300

    agregué un forEach

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 
+
 /*desafío complementario*/
 
 //solicita nombre y apellido al usuario mediante un prompt, con el uso de condicionales para devolver distintos mensajes, dependiendo si llena o no los espacios
@@ -49,6 +50,14 @@ let stockProductos = [
 
 let carritoDeCompras = []
 
+function mostrarProductos() {//muestra los productos en la consola
+    stockProductos.forEach((producto)=>{
+        console.log(producto);
+    })
+}
+
+mostrarProductos()
+
 agregarAlCarrito()
 
 function agregarAlCarrito() {//esta función permite agregar al carrito mi producto, eligiendo por prompt el "id" correspondiente
@@ -61,6 +70,7 @@ function agregarAlCarrito() {//esta función permite agregar al carrito mi produ
     actualizarCarrito()
     
 }
+
 
 function actualizarCarrito() {//mediante esta función se calcula el valor final del producto con el iva y el descuento incluidos
     console.log('cantidad de productos agregados ' + carritoDeCompras.length)
@@ -75,5 +85,67 @@ console.log(existe); //el método some permite saber si existe o no determinado 
 
 const baratos = stockProductos.filter(producto => producto.precio < 400)
 console.log(baratos);
-//el método filter permite mostrar los productos dentro del array, cuyo precio  sea menor a 400
+//el método filter permite mostrar los productos dentro del array, cuyo precio  sea menor a 400 
 
+
+/*
+class Producto {
+    constructor(id, nombre, precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
+const producto1 = new Producto (1, "Javascript", 15000);
+const producto2 = new Producto (2, "React JS", 10000);
+const producto3 = new Producto (3, "Angular", 23000);
+const producto4 = new Producto (4, "Node JS", 30000);
+const producto5 = new Producto (5, "Java", 15000);
+const producto6 = new Producto (6, "Python", 20000);
+const producto7 = new Producto (7, "C#", 23000);
+const producto8 = new Producto (8, "C++", 30000);
+
+const BBDD = [
+    producto1, 
+    producto2, 
+    producto3, 
+    producto4,
+    producto5, 
+    producto6, 
+    producto7, 
+    producto8
+]
+
+const CARRITO = []
+console.log(BBDD);
+
+function renderizarCursos() {
+    BBDD.forEach((el) =>{
+        document.write(`
+            <br>
+            ${el.nombre} <button onclick="comprar()">Comprar</button>
+            <br>
+            <br>`)
+       
+    })
+    document.write(`<button onclick="">Ordenar</button><br>`)
+}
+
+function comprar() {
+    CARRITO.push(BBDD[0])
+}
+
+//comprar(BBDD[0])
+//comprar(BBDD[2])
+//comprar(BBDD[3])
+
+function ordenarPrecio(){
+
+    BBDD.sort((a, b) =>{
+        return a.precio - b.precio
+    })
+}
+ordenarPrecio()
+const idHastaCino = BBDD.filter((curso)=>curso.id <=5);
+console.log(idHastaCino);*/
